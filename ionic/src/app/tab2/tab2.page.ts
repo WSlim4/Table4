79ddf20485e733049e2a6af8f0352d2cbb0cccc6
igu <igor@ejcm.com.br>
2019-07-17 17:53:01 -0300

    +Lista de Pessoas na mesa

--- ionic/src/app/tab2/tab2.page.ts
+++ ionic/src/app/tab2/tab2.page.ts
@@ -6,7 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-
+  pessoas = [
+    {id:1, nome:'Fábio', pagar:'10 reais'},
+    {id:2, nome:'Igor', pagar:'10 reais'},
+    {id:3, nome:'Stephanie', pagar:'10 reais'},
+    {id:4, nome:'Mauro', pagar:'10 reais'},
+    {id:5, nome:'Wesley', pagar:'10 reais'}
+  ]
   constructor() {}
 
 }
