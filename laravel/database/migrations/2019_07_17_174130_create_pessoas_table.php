2b5f914f7715acfb0861f7fbe8a857405d8a3c31
Wesley Lima <wesleyjoga500@gmail.com>
2019-07-17 21:48:14 -0300

    + migration + model pedido

--- laravel/database/migrations/2019_07_17_174130_create_pessoas_table.php
+++ laravel/database/migrations/2019_07_17_174130_create_pessoas_table.php
@@ -19,7 +19,7 @@ class CreatePessoasTable extends Migration
             $table->timestamps();
         });
     }
-
+      
     /**
      * Reverse the migrations.
      *
---
+++ laravel/database/migrations/2019_07_18_003608_create_pedidos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePedidosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pedidos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('pessoa_id')->unsigned();
+            $table->integer('quantidade');
+            $table->float('preco');
+            $table->string('nome');
+            $table->timestamps();
+        });
+    }
+       Schema::table('pedidos', function (Blueprint $table)){
+            $table->foreign('pessoa_id')->references('id')->on('pessoas')->onDelete('cascade');
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pedidos');
+    }
+}
