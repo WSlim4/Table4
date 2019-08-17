2d1c4b086e95ec1fffeebd6ddf231d136501eaa6
Wesley Lima <wesleyjoga500@gmail.com>
2019-08-16 21:30:04 -0300

    + Login Google

--- laravel/app/SocialGoogleAccount.php
+++ laravel/app/SocialGoogleAccount.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class SocialGoogleAccount extends Model
 {
-    //
+    protected $fillable = ['user_id', 'provider_user_id', 'provider'];
+    
+    public function user(){
+      return $this->belongsTo(User::class);
+    }
 }
