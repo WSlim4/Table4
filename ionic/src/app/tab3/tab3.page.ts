9de5f060d335375795d17ed1f27a9cb78db9d757
FabioRNobrega <fabio.r.nobrega@gmail.com>
2019-09-07 18:36:31 -0300

    + overflow e buttons de configuração e alertbasico

--- ionic/src/app/tab3/tab3.page.ts
+++ ionic/src/app/tab3/tab3.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import * as $ from 'jquery';
-
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -9,15 +9,24 @@ import * as $ from 'jquery';
 })
 export class Tab3Page {
   pedidos = [
-    {id:1, nome:'Cerveja', valor:'10 reais'},
-    {id:2, nome:'Batata Frita', valor:'10 reais'},
-    {id:3, nome:'Sorvete', valor:'10 reais'},
-    {id:4, nome:'Hamburguer', valor:'10 reais'},
-    {id:5, nome:'Amendoim', valor:'10 reais'}
+    {id:1, nome:'Cerveja', valor:'10'},
+    {id:2, nome:'Batata Frita com Katshup e Salça com frango', valor:'10'},
+    {id:3, nome:'Palavra Enoooooooooooooooooooooooooooooorme', valor:'10'},
+    {id:4, nome:'Hamburguer', valor:'10'},
+    {id:5, nome:'Amendoim', valor:'10'}
   ]
-  constructor() {}
+  constructor(public alertController: AlertController) {}
 
-changeTextColor(){
-    $('#myButton').text('white');
+    async presentAlert(){
+      const alert = await this.alertController.create({
+        header:'Alert',
+        subHeader:'Subtitle',
+        message: 'This is an alert message',
+        buttons:['OK']
+    });
+
+    await alert.present();
+    let result = await alert.onDidDismiss();
+    console.log(result);
+    }
   }
-}
