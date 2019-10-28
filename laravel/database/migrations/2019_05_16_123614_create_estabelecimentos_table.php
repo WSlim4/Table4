2c5219cd652c1ea1337e3b0b85ac3bd027893a2d
Bruna <brunasantos@ejcm.com.br>
2019-10-28 14:52:58 -0300

    * atributos de estabelecimentos alterados

--- laravel/database/migrations/2019_05_16_123614_create_estabelecimentos_table.php
+++ laravel/database/migrations/2019_05_16_123614_create_estabelecimentos_table.php
@@ -15,7 +15,38 @@ class CreateEstabelecimentosTable extends Migration
     {
         Schema::create('estabelecimentos', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('nome');
+            $table->string('name');
+            $table->text('description');
+            $table->string('site');
+            $table->string('phone');
+            $table->string('email');
+            $table->string('instagram');
+            $table->string('open_hours_monday');
+            $table->string('open_hours_tuesday');
+            $table->string('open_hours_wednesday');
+            $table->string('open_hours_thursday');
+            $table->string('open_hours_friday');
+            $table->string('open_hours_saturday');
+            $table->string('open_hours_sunday');
+            $table->string('address_address');
+            $table->string('address_latitude');
+            $table->string('address_longitude');
+            $table->boolean('parking')->nullable();
+            $table->boolean('reservation')->nullable();
+            $table->boolean('accessible')->nullable();
+            $table->boolean('pet_friendly')->nullable();
+            $table->boolean('couvert_payed')->nullable();
+            $table->boolean('taxa_rolha_payed')->nullable();
+            $table->boolean('credit_cards')->nullable();
+            $table->boolean('qr_code_payment')->nullable();
+            $table->string('reservation_contact')->nullable();
+            $table->string('price_rate');
+            $table->string('couvert_value')->nullable();
+            $table->string('taxa_rolha_value')->nullable();
+            $table->string('type');
+            $table->string('type_details');
+            $table->string('cuisine');
+            $table->string('photo')->nullable();
             $table->timestamps();
         });
     }
