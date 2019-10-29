31da3e41c077cea1213e121a826902365e68638d
Filipe <filipecbs94@gmail.com>
2019-10-29 01:08:14 -0200

    + Integração dos estabelecimentos

--- ionic/src/app/estabelecimentos-detalhe/estabelecimentos-detalhe.page.ts
+++ ionic/src/app/estabelecimentos-detalhe/estabelecimentos-detalhe.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EstabelecimentosService } from '../services/estabelecimentos/estabelecimentos.service';
+import { Router } from '@angular/router';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 @Component({
     selector: 'app-estabelecimentos-detalhe',
@@ -8,23 +10,38 @@ import { EstabelecimentosService } from '../services/estabelecimentos/estabeleci
 })
 export class EstabelecimentosDetalhePage implements OnInit {
 
-    estabelecimentoDetalhe: any[];
+    detalhe: any[];
+    estabelecimentoId;
+    instagram;
 
-    constructor(public estabelecimentosService: EstabelecimentosService) { }
-
-    getEstabelecimento(): void {
-        this.estabelecimentoDetalhe = this.estabelecimentosService.getEstabelecimento();
-        console.log("getEstabelecimento");
-        console.log(this.estabelecimentoDetalhe);
+    constructor(private estabelecimentosService: EstabelecimentosService, private router: Router, private inAppBrowser: InAppBrowser) {
+        this.estabelecimentoId = this.router.getCurrentNavigation().extras;
     }
+
+    // getEstabelecimento(): void {
+    //     this.estabelecimentoDetalhe = this.estabelecimentosService.getEstabelecimento();
+    //     console.log("getEstabelecimento");
+    //     console.log(this.estabelecimentoDetalhe);
+    // }
 
     ngOnInit() {
     }
 
-    ionViewDidEnter() {
-        this.getEstabelecimento();
-        console.log("ionViewDidEnter");
-        console.log(this.estabelecimentoDetalhe);
+    ionViewWillEnter() {
+        this.estabelecimentosService.getEstabelecimento(this.estabelecimentoId).subscribe((res) => {
+            this.detalhe = res.data;
+            console.log(this.detalhe);
+        });
     }
+
+    abrirInstagram() {
+        this.instagram = this.inAppBrowser.create('https://play.google.com/store/apps/details?id=com.instagram.android','_system',{location:'yes'});
+    }
+
+    // ionViewDidEnter() {
+    //     this.getEstabelecimento();
+    //     console.log("ionViewDidEnter");
+    //     console.log(this.estabelecimentoDetalhe);
+    // }
 
 }
--- ionic/src/app/services/estabelecimentos/estabelecimentos.service.ts
+++ ionic/src/app/services/estabelecimentos/estabelecimentos.service.ts
@@ -1,20 +1,33 @@
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment, SERVER_URL } from '../../../environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
 export class EstabelecimentosService {
 
+    url: string = SERVER_URL + 'estabelecimento';
     estabelecimentos: any[];
 
-    constructor() { }
+    constructor(public http: HttpClient) { }
 
-    setEstabelecimento(id, estabelecimentos) {
-        this.estabelecimentos = estabelecimentos[id];
-        console.log(this.estabelecimentos);
+    // setEstabelecimento(id, estabelecimentos) {
+    //     this.estabelecimentos = estabelecimentos[id];
+    //     console.log(this.estabelecimentos);
+    // }
+    //
+    // getEstabelecimento() {
+    //     return this.estabelecimentos;
+    // }
+
+    getEstabelecimentos(): Observable<any> {
+        return this.http.get(SERVER_URL + 'estabelecimento/').pipe(map(res => res));
     }
 
-    getEstabelecimento() {
-        return this.estabelecimentos;
+    getEstabelecimento(id: number): Observable<any> {
+        return this.http.get(SERVER_URL + 'estabelecimento/' + id).pipe(map(res => res));
     }
 }
